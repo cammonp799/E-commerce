70188dc6829498fceeb525c22cbdee4a04ff7d95
junior_yoomi <junior.chimene@yoomi-solution.com>
2025-07-01 13:14:43 +0200

    Avant mise à jour vers Angular 18

---
+++ src/app/heroes/hero-carrousel/hero-carrousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-carrousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-carrousel.component.html',
+  styleUrl: './hero-carrousel.component.scss'
+})
+export class HeroCarrouselComponent {
+
+}
