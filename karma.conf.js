f7bcedbdcf10c5902ad1f11e555b5f4cb65898fe
junior_yoomi <junior.chimene@yoomi-solution.com>
2025-08-21 17:06:37 +0200

    add

--- karma.conf.js
+++ karma.conf.js
@@ -3,14 +3,22 @@ process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
-    // ... (garde le reste tel quel)
+    // ... garde tes settings existants (frameworks, files, reporters, coverage, etc.)
     browsers: ['ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+        flags: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+        ],
       },
     },
-    singleRun: true,
+    singleRun: true,           // important en CI (pas de watch)
+    // builder selon ta version d’Angular :
+    // Angular 18 -> builder @angular-devkit/build-angular
+    // Angular 20 -> builder @angular/build
   });
 };
